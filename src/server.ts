db28bc350c7d20eccc85209b48714fcdf49708c7
Aldair-da-cruz <aldairfrcruz@gmail.com>
2026-05-18 01:27:14 +0100

    deploy config supabase

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,29 @@ import { prisma } from '@/shared/database/prisma.client'
 
 const PORT = process.env.PORT ?? 3333
 
-app.listen(PORT, () => {
-  console.log(`
+// app.listen(PORT, () => {
+//   console.log(`
   
-     VAR API              
-     Servidor rodando na porta ${PORT}      
-     http://localhost:${PORT}/api/v1        
-     http://localhost:${PORT}/health
-  `)
-})
+//      VAR API              
+//      Servidor rodando na porta ${PORT}      
+//      http://localhost:${PORT}/api/v1        
+//      http://localhost:${PORT}/health
+//   `)
+// })
 
 async function main() {
   await prisma.$connect() // falha aqui se a DB não estiver acessível
-  app.listen(PORT, () => console.log(`Servidor na porta ${PORT}`))
+  app.listen(PORT, () => {
+    console.log(`
+    
+       VAR API              
+       Servidor rodando na porta ${PORT}      
+       http://localhost:${PORT}/api/v1        
+       http://localhost:${PORT}/health
+    `)
+  })
 }
-main().catch((e) => { console.error(e); process.exit(1) })
+main().catch((e) => {
+   console.error(e); 
+   process.exit(1)
+})
